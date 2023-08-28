c44dec845efdaf6fb4e5f06f58976f740e46a192
JeaLousy1337 <timurka.t.r@gmail.com>
2023-08-28 12:18:07 +0300

    modified func files

--- homeworkfunc25.py
+++ homeworkfunc25.py
@@ -1,10 +1,12 @@
-def car_description(*, time: int | float, speed: int | float, weight: int | float) -> str:
+def car_description(*, time: int | float, speed: int | float, weight: int | float, distance: int | float) -> str:
     if time < 0:
         raise ValueError('Values must be absolute')
     elif speed < 0:
         raise ValueError('Values must be absolute')
     elif weight < 0:
         raise ValueError('Values must be absolute')
+    elif distance < 0:
+        raise ValueError('Values must be absolute')
 
-    result = f"A vehicle weighing {weight} kg moved for {time} seconds at a speed of {speed} m/s and covered a distance of 30 meters"
+    result = f"A vehicle weighing {weight} kg moved for {time} seconds at a speed of {speed} m/s and covered a distance of {distance} meters"
     return result
