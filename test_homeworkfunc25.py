6173cda7ce4d9c31bc054fe2498fc0e316d0bc0c
JeaLousy1337 <timurka.t.r@gmail.com>
2023-08-28 17:58:16 +0300

    hw func: car description last try

--- test_homeworkfunc25.py
+++ test_homeworkfunc25.py
@@ -3,11 +3,11 @@ from homeworkfunc25 import car_description
 
 def test_car_description():
     expected = 'A vehicle weighing 1000 kg moved for 10 seconds at a speed of 3 m/s and covered a distance of 30 meters'
-    actual = car_description(time=10, speed=3, weight=1000, distance=30)
+    actual = car_description(time=10, speed=3, weight=1000)
     assert actual == expected
 
 
 def test_second_car_description():
     expected = 'A vehicle weighing 1200 kg moved for 10 seconds at a speed of 4 m/s and covered a distance of 40 meters'
-    actual = car_description(time=10, speed=4, weight=1200, distance=40)
+    actual = car_description(time=10, speed=4, weight=1200)
     assert actual == expected
