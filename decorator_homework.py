44d46ca0358f3bb1b8a39c7e66942e627fe29464
JeaLousy1337 <timurka.t.r@gmail.com>
2023-09-15 18:02:52 +0300

    decorator homework is done

--- decorator_homework.py
+++ decorator_homework.py
@@ -1,10 +1,11 @@
 # decorator homework
 
-def decorator_key_word_as_str(words):
-    def operation_deco(*args):
-        operation = words(*args)
+def decorator_key_word_as_str(func):
+    def operation_deco(*args, **kwargs):
+        operation = func(*args, **kwargs)
         operation = str(operation)
         print(*args)
+        print(kwargs)
         return operation
 
     return operation_deco
@@ -15,5 +16,5 @@ def final_word(text1: str, text2: str) -> str:
     return text1 + text2
 
 
-result = final_word('при', 'віт')
+result = final_word(text1='при', text2='віт')
 print(result)
