9c5841101971ffd912b5bcb49c270c6b1b4290ea
JeaLousy1337 <timurka.t.r@gmail.com>
2023-09-24 21:58:07 +0300

    oop is modified

--- oop777.py
+++ oop777.py
@@ -14,10 +14,51 @@ class Car:
                f'\n run: {self.run}' \
                f'\n fuel_consumption: {self.fuel_consumption}'
 
+    def __del__(self):
+        with open('cars_file_csv.csv', mode='a', encoding='utf-8') as file:
+            file.write(f'{self.producer};{self.brand};{self.graduation_year}\n')
+
+
+class Truck(Car):
+    def __init__(self, carrying_capacity: int):
+        super().__init__(producer='Lamborghini', brand='Lambo-Truck', fuel_consumption=18.8)
+        self.carrying_capacity = carrying_capacity
+
+    def __str__(self):
+        return f'Сharacteristic of the {self.__class__.__name__}: ↓' \
+               f'\n graduation year: {self.graduation_year}' \
+               f'\n producer: {self.producer}' \
+               f'\n brand: {self.brand}' \
+               f'\n run: {self.run}' \
+               f'\n fuel_consumption: {self.fuel_consumption}' \
+               f'\n carrying_capacity: {self.carrying_capacity}'
+
+
+class SportCar(Car):
+    def __init__(self, speed: int, price: int = 12900):
+        super().__init__(producer='Lanos', brand="Lada", fuel_consumption=16.8)
+        self.speed = speed
+        self.price = price
+
+    def __str__(self):
+        return f'Сharacteristic of the {self.__class__.__name__}: ↓' \
+               f'\n graduation year: {self.graduation_year}' \
+               f'\n producer: {self.producer}' \
+               f'\n brand: {self.brand}' \
+               f'\n run: {self.run}' \
+               f'\n fuel_consumption: {self.fuel_consumption}' \
+               f'\n speed: {self.speed}' \
+               f'\n price: {self.price}'
+
 
 auto = Car(producer='BMW', brand="Rolls-Royce", fuel_consumption=15.2)
 auto2 = Car(producer='Volkswagen', brand="Porsche", fuel_consumption=10.9, graduation_year=2077)
 auto3 = Car(producer='Toyota', brand="Lexus", fuel_consumption=16.8, graduation_year=2023)
-print(auto)
-print(auto2)
-print(auto3)
+auto4 = Truck(carrying_capacity=200)
+auto5 = SportCar(speed=350, price=120000)
+
+del auto
+del auto2
+del auto3
+del auto4
+del auto5
