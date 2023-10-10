98ca5b9ae6169667eeffbf00dde1414b9cd3b036
JeaLousy1337 <timurka.t.r@gmail.com>
2023-10-10 22:30:38 +0300

    second dating with db

--- mongo_db.py
+++ mongo_db.py
@@ -2,6 +2,7 @@ import pymongo
 
 from config import USER, PASSWORD
 
+from bson import Decimal128
 url = f'mongodb+srv://{USER}:{PASSWORD}' \
       f'@cluster0.xdd2fq6.mongodb.net/?retryWrites=true&w=majority'
 
@@ -20,9 +21,23 @@ vacuum_cleaners_coll = db.vacuum_cleaners
 #     {'title': 'vacuum cleaner', 'price': 150,
 #      'power': 2000}
 # )
-
+#
 # mops_coll.insert_one(
 #       {'_id': 2, 'title': 'Super mop', 'price': 15}
 # )
 # add single document ####################
 
+# mops_coll.insert_one(
+#       {'title': 'Super puper mop', 'price': 133}
+# )
+
+# query = {}
+# operation = {'$mul': {'price': Decimal128(str(1.1))}}
+#
+# data = mops_coll.update_many(query, operation)
+# print(data.raw_result)
+#
+# mops_coll.drop()
+# vacuum_cleaners_coll.drop()
+#
+# client.drop_database('testDB')
